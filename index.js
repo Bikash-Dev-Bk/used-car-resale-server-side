9b4dcabc8b081fc351f39c16c0a5ab28f07441c2
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-25 21:15:38 +0600

    get products data from mongodb

--- index.js
+++ index.js
@@ -13,14 +13,26 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nfyjflh.mongodb.net/?retryWrites=true&w=majority`;
-
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        const productCollection = client.db("gearUP").collection("products");
+        app.get("/products", async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+  
+    } 
+  
+    finally {
+  
+    }
+  }
+  
+  run().catch((err) => console.error(err));
 
 
 
