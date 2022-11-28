2982ee7fd8c5a708fa21c8e355463aa95b6cfb11
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-28 23:49:48 +0600

    add product funtionality added

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ async function run() {
         const categoryCollection = client.db("gearUP").collection("categories");
         const serviceCollection = client.db("gearUP").collection("services");
         const productCollection = client.db("gearUP").collection("products");
-
-
         const userCollection = client.db("gearUP").collection("usersInfo");
+        // const bookingsCollection = client.db("gearUP").collection("bookings");
+
 
         app.get("/users", async (req, res) => {
             const query = {};
@@ -64,12 +64,35 @@ async function run() {
             res.send(products);
         });
 
+         app.post("/category/products", async (req, res) => {
+            const product = req.body;
+            console.log(product);
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        });
+
+
         app.get("/services", async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        // app.get("/bookings", async (req, res) => {
+        //     const email = req.query.email;
+        //     const query = { email: email };
+        //     const bookings = await bookingsCollection.find(query).toArray();
+
+        //     res.send(bookings);
+        // });
+
+        // app.post("/bookings", async (req, res) => {
+        //     const booking = req.body;
+        //     console.log(booking);
+        //     const result = await bookingsCollection.insertOne(booking);
+        //     res.send(result);
+        // });
     } 
   
     finally {
